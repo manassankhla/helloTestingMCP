702819eddcfc0ad825d4730be2a3472b0d75c55d
MANASSANKHLA <MANASSANKHLA991@GMAIL.COM>
2026-06-27 11:30:28 +0530

    openai doc according

--- app/widget/page.tsx
+++ app/widget/page.tsx
@@ -1,19 +1,19 @@
-"use client";
+    "use client";
 
-export default function Widget() {
-  return (
-    <div
-      style={{
-        padding: 30,
-        textAlign: "center",
-        fontFamily: "sans-serif",
-        backgroundColor: "#ff7d7d",
-        color: "#333",
-      }}
-    >
-      <h1>Hello Manas 👋</h1>
+    export default function Widget() {
+    return (
+        <div
+        style={{
+            padding: 30,
+            textAlign: "center",
+            fontFamily: "sans-serif",
+            backgroundColor: "#ff7d7d",
+            color: "#333",
+        }}
+        >
+        <h1>Hello Manas 👋</h1>
 
-      <p>How are you today?</p>
-    </div>
-  );
-}
+        <p>How are you today?</p>
+        </div>
+    );
+    }
